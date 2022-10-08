4ca526958bb32fdab8eb9b9b08f847c96f9f16d4
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-08 20:00:38 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/authentication/local-authentication.spec.ts
+++ src/data/usecases/authentication/local-authentication.spec.ts
@@ -1,11 +1,23 @@
 import { LocalStorageGetClientSpy } from '../../test/mock-local-storage-client';
 import { LocalAuthentication } from './local-authentication';
 
+type SutTypes = {
+  sut: LocalAuthentication;
+  localGetClientSpy: LocalStorageGetClientSpy;
+};
+
+// factory
+const makeSut = (key: string = '@any_key'): SutTypes => {
+  const localGetClientSpy = new LocalStorageGetClientSpy();
+  const sut = new LocalAuthentication(key, localGetClientSpy);
+
+  return { sut, localGetClientSpy };
+};
+
 describe('LocalAuthentication', () => {
   test('Should call LocalStorageClient with a specific key', async () => {
     const key = '@user_email';
-    const localGetClientSpy = new LocalStorageGetClientSpy();
-    const sut = new LocalAuthentication(key, localGetClientSpy);
+    const { sut, localGetClientSpy } = makeSut(key);
     await sut.auth();
     expect(localGetClientSpy.key).toBe(key);
   });
