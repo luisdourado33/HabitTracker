d52cc442b0e8248c4916a3a8721b8bc1176ea210
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-08 12:00:09 -0300

    test: add mockHabitModel mock

--- tests/domain/mocks/mock-habit.ts
+++ tests/domain/mocks/mock-habit.ts
@@ -1,1 +1,10 @@
+import { HabitModel } from '../../../src/domain/models';
+
 import { faker } from '@faker-js/faker';
+
+export const mockHabitModel = (): HabitModel => ({
+  title: faker.random.words(),
+  goal: 'Complete this task :)',
+  deadline: faker.date.future(),
+  isCompleted: faker.datatype.boolean(),
+});
