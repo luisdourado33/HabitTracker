a40e72dfc541d51d3e83c0cd6290b409fd8d4170
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-11 17:14:45 -0300

    feat: add SplashScreen factory

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
+
 import { makeNativeStackNavigatorFactory } from '../factories/navigator';
+import { makeSplashScreen } from '../factories/pages';
 
 const Stack = makeNativeStackNavigatorFactory();
 
 export const Router: React.FC<any> = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator></Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="SplashScreen" component={makeSplashScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
