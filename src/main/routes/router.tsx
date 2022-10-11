291a8d54940f7308d43631262ebd5bd3329a2e25
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-11 15:30:04 -0300

    chore(deps): add react-native-screens react-native-safe-area-context

---
+++ src/main/routes/router.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+export const Router: React.FC<any> = () => {
+  return (
+    <View>
+      <Text>Router component</Text>
+    </View>
+  );
+};
