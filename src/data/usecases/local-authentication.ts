ad348c27d8cba8cef9de71cce180cc41d6dbe58d
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-11 14:02:47 -0300

    feat: add LocalAuthentication usecase

---
+++ src/data/usecases/local-authentication.ts
@@ -0,0 +1,20 @@
+import {Authentication} from '../../domain/usecases';
+import {LocalStorageClient} from '../protocols/cache';
+
+import {EmptyValueError} from '../errors';
+
+export class LocalAuthentication implements Authentication {
+  constructor(
+    private readonly localStorageClient: LocalStorageClient<Authentication.Model>,
+  ) {}
+
+  async auth(params: Authentication.Params): Promise<Authentication.Model> {
+    const cachedUser = await this.localStorageClient.get(params.key);
+
+    if (cachedUser == null) {
+      throw new EmptyValueError();
+    }
+
+    return cachedUser;
+  }
+}
