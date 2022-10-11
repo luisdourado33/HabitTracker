d83e4917abf2fa264af0b2c66d906e2e29eb2b39
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-10 22:56:03 -0300

    refactor: add log to methods from infra

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -3,13 +3,17 @@ import { LocalStorageClient } from '@/data/protocols/cache';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export class LocalStorageAdapter implements LocalStorageClient {
+  name = 'LocalStorageAdapter';
   async get(key: string): Promise<any> {
     const response = await AsyncStorage.getItem(key);
+    console.log(`[${this.name}] getting item from AsyncStorage`);
+
     return response;
   }
 
-  async set(key: string, value: any): Promise<any> {
+  async set(key: string, value: any): Promise<void> {
     const response = await AsyncStorage.setItem(key, value);
+    console.log(`[${this.name}] saving item in AsyncStorage`);
     return response;
   }
 }
