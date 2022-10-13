3e1f3e3793803f07451edd484be03c04e5535b64
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-13 15:33:02 -0300

    chore: format some files

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { makeFirstPage } from './src/main/factories/pages/first-page-factory';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => makeFirstPage);
