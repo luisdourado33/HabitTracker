704105adbdc216be266694ceaddd02f126848434
luisdourado33 <luis_dourado33@hotmail.com>
2022-10-15 20:46:38 -0300

    feat: add FirstSetupScreen to route map

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,15 +4,23 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { makeNativeStackNavigatorFactory } from '../factories/navigator';
 import { makeSplashScreen } from '../factories/pages';
+import { makeFirstSetupScreen } from '../factories/pages/first-setup-screen';
 
 const Stack = makeNativeStackNavigatorFactory();
 
 export const Router: React.FC<any> = () => {
-  const navigatorOptions = { headerShown: false };
+  const navigatorOptions = {
+    headerShown: false,
+    defaultScreen: 'FirstSetupScreen',
+  };
 
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={navigatorOptions}>
+        <Stack.Screen
+          name="FirstSetupScreen"
+          component={makeFirstSetupScreen}
+        />
         <Stack.Screen name="SplashScreen" component={makeSplashScreen} />
       </Stack.Navigator>
     </NavigationContainer>
